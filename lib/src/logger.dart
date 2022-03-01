0343b71316f7b3c3f83724b516487a7dc9669b87
Manuel Calleriza <manuelcalleriza@gmail.com>
2022-03-01 16:38:53 -0300

    Feature/logger (#22)
    * logger: (WIP) log history added
    
    * logger: added description for historyHandler
    
    * logger: (WIP) Buffer option for logger history
    
    * logger: setter and getter functions for buffer implemented

--- lib/src/logger.dart
+++ lib/src/logger.dart
@@ -1,8 +1,14 @@
 import 'package:logger/logger.dart';
 
 Logger getLogger(String className) {
-  return Logger(printer: InternalLogPrinter(className));
+  var output = MultiOutput([memory, ConsoleOutput()]);
+  return Logger(output: output, printer: InternalLogPrinter(className));
 }
+
+BufferMemoryOutput memory = BufferMemoryOutput(bufferSize: maxLogHistorySize);
+const defaultLogHistorySize = 5;
+int maxLogHistorySize = defaultLogHistorySize;
+List<String> history = [];
 
 //LogPrinter implementation
 //Log format '[className] Date - {Log.level} - {log.message}'
@@ -13,10 +19,53 @@ class InternalLogPrinter extends LogPrinter {
   @override
   List<String> log(LogEvent event) {
     AnsiColor? color = PrettyPrinter.levelColors[event.level];
-    return [
-      color!(
-          // ignore: lines_longer_than_80_chars
-          '[$className] ${DateTime.now()} - ${(event.level).toString().toUpperCase().split('.')[1]} - ${event.message}')
-    ];
+    var logResult = color!(
+        // ignore: lines_longer_than_80_chars
+        '[$className] ${DateTime.now()} - ${(event.level).toString().toUpperCase().split('.')[1]} - ${event.message}');
+    OutputEvent output = OutputEvent(event.level, [logResult]);
+    memory.output(output);
+    return [logResult];
+  }
+}
+
+class BufferMemoryOutput extends MemoryOutput {
+  @override
+  // ignore: overridden_fields
+  int bufferSize;
+  BufferMemoryOutput({required this.bufferSize, LogOutput? secondOutput})
+      : super(bufferSize: bufferSize, secondOutput: secondOutput);
+
+  void setBufferSize(int val) {
+    bufferSize = val;
+    if (buffer.length > bufferSize) {
+      while (buffer.length > bufferSize) {
+        buffer.removeFirst();
+      }
+    }
+  }
+
+  List<String> getBuffer() {
+    history = [];
+    for (var element in memory.buffer) {
+      {
+        history.add(element.lines.first.toString());
+      }
+    }
+    return history;
+  }
+
+  int getBufferMaxSize() {
+    return bufferSize;
+  }
+
+  @override
+  void output(OutputEvent event) {
+    if (buffer.length >= bufferSize) {
+      while (buffer.length >= bufferSize) {
+        buffer.removeFirst();
+      }
+    }
+    buffer.addAll([event]);
+    secondOutput?.output(event);
   }
 }
