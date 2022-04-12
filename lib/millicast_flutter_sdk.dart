6ae8ee2b9556280673532d0b497a1f514412b353
fcancela <fabian.cancelafc@gmail.com>
2022-04-12 12:45:07 -0300

    Initial API Reference documentation

--- lib/millicast_flutter_sdk.dart
+++
@@ -1,6 +0,0 @@
-export 'src/director.dart';
-export 'src/publish.dart';
-export 'src/view.dart';
-export 'src/signaling.dart';
-export 'src/peer_connection.dart';
-export 'src/logger.dart';
--- lib/src/config.dart
+++
@@ -1,9 +0,0 @@
-class Config {
-  static const String millicastDirectorEndpoint =
-      'https://director.millicast.com';
-  static const String millicastTurnserverLocation =
-      'https://turn.millicast.com/webrtc/_turn';
-  static const String millicastEventsLocation =
-      'wss://streamevents.millicast.com/ws';
-  static const String? millicastFixedAccountId = '';
-}
