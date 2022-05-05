d4211c8c802b94ea9ee9fa0e7a6124512f188b57
Fabián Cancela <62720589+fcancela@users.noreply.github.com>
2022-05-05 12:49:31 -0300

    h264-support: solved codec fallback codec issue. (#17)
    * h264-support: solved codec fallback codec issue.
    
    * fallback-supported-codec: code review corrections

--- example/lib/publisher_widget.dart
+++ example/lib/publisher_widget.dart
@@ -129,11 +129,8 @@ class _PublisherWidgetState extends State<PublisherWidget>
   }
 
   _setSupportedCodecs() async {
-    var codecObjects = (await PeerConnection.getCapabilities('video'))['codec'];
-    List<String> codecs = [];
-    for (var codec in codecObjects) {
-      codecs.add((codec as String).toLowerCase());
-    }
+    List<String> codecs =
+        (await PeerConnection.getCapabilities('video'))['codec'];
     setState(() {
       _supportedCodecs = codecs;
     });
